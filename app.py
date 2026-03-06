31e2a6dfb20c167c131d9ef3846aeda056f569d1
takahiko02991 <taka02991@gmail.com>
2026-03-07 02:34:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.set_page_config(page_title="休サイト - 休憩の匿名掲示板", page_ic
 st.write("休サイトは、誰でも気軽に書き込める完全匿名の掲示板アプリです。")
 
 # --- 管理者認証システム ---
-ADMIN_PASSWORD = "your_secret_password" # ここに好きなパスワードを設定
+ADMIN_PASSWORD = "fuck_you12345" # ここに好きなパスワードを設定
 
 if "admin_mode" not in st.session_state:
     st.session_state.admin_mode = False
